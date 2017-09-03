5409ebc8affca9768ef915d84bdc61157631a688
James Yin <ywhjames@hotmail.com>
2017-09-04 07:07:31 +0800

    update for indent.

--- src/word2vec.c
+++ src/word2vec.c
@@ -570,22 +570,21 @@ void *TrainModelThread(void *id) {
       //  --+----------+-----------o-----------+----------+--
       // pos-win                  pos                  pos+win
       for (a = b; a < window * 2 + 1 - b; a++) {
-        if (a != window) { // 上下文不包含中心词
-          c = sentence_position - window + a;
+        if (a == window) continue; // 上下文不包含中心词
+        c = sentence_position - window + a;
 
-          // 不在句子范围
-          if (c < 0) continue;
-          if (c >= sentence_length) continue;
+        // 不在句子范围
+        if (c < 0) continue;
+        if (c >= sentence_length) continue;
 
-          last_word = sen[c];
-          if (last_word == -1) continue; // 不在词表中
+        last_word = sen[c];
+        if (last_word == -1) continue; // 不在词表中
 
-          l1 = last_word * layer1_size;
+        l1 = last_word * layer1_size;
 
-          // SUM of syn0(word vector)
-          for (c = 0; c < layer1_size; c++)
-            neu1[c] += syn0[c + l1];
-        }
+        // SUM of syn0(word vector)
+        for (c = 0; c < layer1_size; c++)
+          neu1[c] += syn0[c + l1];
       }
 
       // sigmoid and its derivative:
@@ -805,81 +804,80 @@ void *TrainModelThread(void *id) {
     } else {  //train skip-gram
       // 遍历上下文
       for (a = b; a < window * 2 + 1 - b; a++) {
-        if (a != window) { // 上下文不包含中心词
-          c = sentence_position - window + a;
+        if (a != window) continue; // 上下文不包含中心词
+        c = sentence_position - window + a;
 
-          // 不在句子范围
-          if (c < 0) continue;
-          if (c >= sentence_length) continue;
+        // 不在句子范围
+        if (c < 0) continue;
+        if (c >= sentence_length) continue;
 
-          last_word = sen[c];
-          if (last_word == -1) continue; // 不在词表中
+        last_word = sen[c];
+        if (last_word == -1) continue; // 不在词表中
 
-          l1 = last_word * layer1_size;
+        l1 = last_word * layer1_size;
 
-          for (c = 0; c < layer1_size; c++)
-            neu1[c] = syn0[c + l1];
+        for (c = 0; c < layer1_size; c++)
+          neu1[c] = syn0[c + l1];
 
-          for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
+        for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
 
-          // HIERARCHICAL SOFTMAX
-          if (hs) {
-            for (d = 0; d < vocab[word].codelen; d++) {
-              l2 = vocab[word].point[d] * layer1_size;
+        // HIERARCHICAL SOFTMAX
+        if (hs) {
+          for (d = 0; d < vocab[word].codelen; d++) {
+            l2 = vocab[word].point[d] * layer1_size;
 
-              // Propagate hidden -> output
-              f = 0;
-              for (c = 0; c < layer1_size; c++)
-                f += neu1[c] * syn1[c + l2];
-              if (f <= -MAX_EXP || f >= MAX_EXP) continue;
-              f = sigmoid(f);
+            // Propagate hidden -> output
+            f = 0;
+            for (c = 0; c < layer1_size; c++)
+              f += neu1[c] * syn1[c + l2];
+            if (f <= -MAX_EXP || f >= MAX_EXP) continue;
+            f = sigmoid(f);
 
-              // 'g' is the gradient multiplied by the learning rate
-              g = (1 - vocab[word].code[d] - f) * alpha;
+            // 'g' is the gradient multiplied by the learning rate
+            g = (1 - vocab[word].code[d] - f) * alpha;
 
-              // Propagate errors output -> hidden
-              for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1[c + l2];
-              // Learn weights hidden -> output
-              for (c = 0; c < layer1_size; c++) syn1[c + l2] += g * neu1[c];
-            }
+            // Propagate errors output -> hidden
+            for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1[c + l2];
+            // Learn weights hidden -> output
+            for (c = 0; c < layer1_size; c++) syn1[c + l2] += g * neu1[c];
           }
-
-          // NEGATIVE SAMPLING
-          if (negative > 0) {
-            for (d = 0; d < negative + 1; d++) {
-              if (d == 0) {
-                target = word;
-                label = 1;
-              } else {
-                next_random =
-                    next_random * (unsigned long long) 25214903917 + 11;
-                target = table[(next_random >> 16) % table_size];
-                if (target == 0) target = next_random % (vocab_size - 1) + 1;
-                if (target == word) continue;
-                label = 0;
-              }
-              l2 = target * layer1_size;
-
-              f = 0;
-              for (c = 0; c < layer1_size; c++)
-                f += neu1[c] * syn1neg[c + l2];
-
-              if (f > MAX_EXP) {
-                g = (label - 1) * alpha;
-              } else if (f < -MAX_EXP) {
-                g = (label - 0) * alpha;
-              } else {
-                g = (label - sigmoid(f)) * alpha;
-              }
-
-              for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
-              for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * neu1[c];
-            }
-          }
-
-          // Learn weights input -> hidden
-          for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
         }
+
+        // NEGATIVE SAMPLING
+        if (negative > 0) {
+          for (d = 0; d < negative + 1; d++) {
+            if (d == 0) {
+              target = word;
+              label = 1;
+            } else {
+              next_random =
+                  next_random * (unsigned long long) 25214903917 + 11;
+              target = table[(next_random >> 16) % table_size];
+              if (target == 0) target = next_random % (vocab_size - 1) + 1;
+              if (target == word) continue;
+              label = 0;
+            }
+            l2 = target * layer1_size;
+
+            f = 0;
+            for (c = 0; c < layer1_size; c++)
+              f += neu1[c] * syn1neg[c + l2];
+
+            if (f > MAX_EXP) {
+              g = (label - 1) * alpha;
+            } else if (f < -MAX_EXP) {
+              g = (label - 0) * alpha;
+            } else {
+              g = (label - sigmoid(f)) * alpha;
+            }
+
+            for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
+            for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * neu1[c];
+          }
+        }
+
+        // Learn weights input -> hidden
+        for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
       }
     }
 
